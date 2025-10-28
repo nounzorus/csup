e47b2b9542f5e10d02305133fd3632fa2889a5b9
Arnaud DUSSURGET <noun@vps-6e54585b.vps.ovh.net>
2025-10-28 03:24:58 +0100

    Remove Backend directory

--- backend/src/api/genre/routes/genre.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * genre router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::genre.genre');
--- backend/src/api/genre/services/genre.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * genre service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::genre.genre');
